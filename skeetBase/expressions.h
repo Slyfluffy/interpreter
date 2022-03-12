d81a1fd58f231abff53597de839dcacc3183382e
Ethan Jamison <range.picker@gmail.com>
2022-03-12 09:47:42 -0700

    Create expressions.h

---
+++ skeetBase/expressions.h
@@ -0,0 +1,22 @@
+
+#include "context.h"
+#include "skeet.h"
+#include "birds.h"
+
+#include <List>
+using namespace std;
+
+
+class expressions() {
+  virtual void intrepret(context) = 0;
+};
+
+class createBirds() {
+  List<birds> lvl1birds;
+  List<birds> lvl2birds;
+  List<birds> lvl3birds;
+  Expressions expression;
+  double size;
+  
+  void drawBird(
+};
