349598baedbe5c5928864df54265130753bdd6fc
Ethan Jamison <range.picker@gmail.com>
2022-03-12 09:35:17 -0700

    Create context.cpp

---
+++ skeetBase/context.cpp
@@ -0,0 +1,13 @@
+
+
+#include expressions.h
+
+class context() {
+  private:
+    List<Expressions> expressions;
+    int index;
+  
+  public:
+    String getNext(index) {};
+    void advanceIndex() { index++; return; };
+}
